0dbe235d04aaa83501e8fce50eaf64d4801fe26d
denartha10 <mahonniall2012@gmail.com>
2025-07-19 02:30:25 +0100

    feat: implement DefaultMatchRanking class to compare matches by score, start time, and team names

---
+++ src/main/java/com/sportsradar/DefaultMatchRanking.java
@@ -0,0 +1,22 @@
+package com.sportsradar;
+
+import java.util.Comparator;
+
+@SuppressWarnings("ReassignedVariable")
+public class DefaultMatchRanking implements Comparator<Match> {
+    @Override
+    public int compare(Match o1, Match o2) {
+        // compare by total score first
+        int scoreComparison = Integer.compare(o2.getHomeScore() + o2.getAwayScore(), o1.getHomeScore() + o1.getAwayScore());
+
+        // if scores are equal, compare by start time
+        if (scoreComparison == 0) scoreComparison = Long.compare(o1.getStartTime(), o2.getStartTime());
+
+        // if still equal, compare by team names
+        if (scoreComparison == 0) scoreComparison =
+                (o1.getHomeTeam() + o1.getAwayTeam())
+                .compareTo(o2.getHomeTeam() + o2.getAwayTeam());
+
+        return scoreComparison;
+    }
+}
